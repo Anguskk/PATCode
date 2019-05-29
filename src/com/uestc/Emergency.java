5edbd910fc2adbe6a1122f0595906869be6aba46
AngusKK <604202346@qq.com>
2019-05-29 16:06:03 +0800

    Emergency 3case pass still 3case deny

--- src/com/uestc/Emergency.java
+++ src/com/uestc/Emergency.java
@@ -1,5 +1,4 @@
 package com.uestc;
-
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -7,34 +6,39 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Emergency {
-        private Node[] vNodes;
-        private int[] distTo;
-        private int[] NodeWeights;
-        private ArrayList<Edge>[] adjedges;
-        static int cnt=0;
+    private Node[] vNodes;
+    private int[] distTo;
+    private int[] rescueTeams;
+    private int[] roads;
+    private ArrayList<Edge>[] adjedges;
+    //static int cnt=0;
 
     public Emergency(int V,int s){
 
-            vNodes = new Node[V];
-            distTo = new int[V];
-            NodeWeights =new int[V];
+        vNodes = new Node[V];
+        distTo = new int[V];
+        rescueTeams =new int[V];
+        roads = new int[V];
+        for (int i = 0; i <V ; i++) {
+            roads[i] = 0;
+        }
+        roads[s] = 1;
         for (int i = 0; i <V ; i++) {
             distTo[i] = Integer.MAX_VALUE;
 
-
         }
-            distTo[s] =0 ;
-            adjedges = (ArrayList<Edge>[]) new ArrayList[V];
-            for (int i = 0; i <V ; i++) {
-                adjedges[i] =new ArrayList<Edge>();
-            }
+        distTo[s] =0 ;
+        adjedges = (ArrayList<Edge>[]) new ArrayList[V];
+        for (int i = 0; i <V ; i++) {
+            adjedges[i] =new ArrayList<Edge>();
         }
-        public void setNodesweight(int[] tNW){
-            for (int i = 0; i <vNodes.length ; i++) {
-                int w=tNW[i];
-                vNodes[i] = new Node(i,w);
-                NodeWeights[i]=w;
-            }
+    }
+    public void setNodesweight(int[] tNW){
+        for (int i = 0; i <vNodes.length ; i++) {
+            int w=tNW[i];
+            vNodes[i] = new Node(i,w);
+            rescueTeams[i]=w;
+        }
 
     }
     public Iterable<Edge> adj(int v) {
@@ -60,10 +64,21 @@ public class Emergency {
         int Edgeweight;
         int n1;
         int n2;
+        boolean isRelax;
+
+        public void setRelax(boolean isrelax) {
+            isRelax = isrelax;
+        }
+
+        public boolean isRelax() {
+            return isRelax;
+        }
+
         Edge(int s,int t,int w){
             this.n1=s;
             this.n2=t;
             this.Edgeweight= w;
+            isRelax = false;
         }
         int theOther(int temp){
             if (temp == n1) {
@@ -73,25 +88,25 @@ public class Emergency {
         }
 
     }
-    public void relax(int v1,Edge e,int v2){
+    public void relax(int v1,Edge e){
         if (distTo[e.theOther(v1)] > distTo[v1]+e.Edgeweight) {
             distTo[e.theOther(v1)] = distTo[v1]+e.Edgeweight;
-            if (e.theOther(v1)==v2){
-                cnt =1;
-            }
-            NodeWeights[e.theOther(v1)] += vNodes[v1].nodeWeight;
+            roads[e.theOther(v1)] = roads [v1];
+            rescueTeams[e.theOther(v1)] = rescueTeams[v1]+vNodes[e.theOther(v1)].nodeWeight;
         }
         else if (distTo[e.theOther(v1)] == distTo[v1]+e.Edgeweight){
-            if (e.theOther(v1)==v2)    cnt += 1;
-            if (NodeWeights[e.theOther(v1)] < vNodes[e.theOther(v1)].nodeWeight+ NodeWeights[v1] ) {
-                NodeWeights[e.theOther(v1)] = vNodes[e.theOther(v1)].nodeWeight+ NodeWeights[v1];
-            }
+            roads[e.theOther(v1)] += roads[v1];
+            rescueTeams[e.theOther(v1)] = Math.max(rescueTeams[v1]+vNodes[e.theOther(v1)].nodeWeight,rescueTeams[e.theOther(v1)]);
+
         }
 
     }
+    public int getRoads(int i) {
+        return roads[i];
+    }
 
-    public int getNodeWeight(int v){
-        return NodeWeights[v];
+    public int getRescueTrams(int v){
+        return rescueTeams[v];
     }
 
     public static void main(String[] args){
@@ -115,24 +130,37 @@ public class Emergency {
             my.setEdge(start,to,weight);
         }
         //数据读入完成
-        Queue<Integer> queue = new LinkedList<Integer>();
-        boolean[] isvisit=new boolean[n];
-        queue.offer(c1);
-        isvisit[c1]=true;
-        while(!queue.isEmpty()){
-            int num=queue.poll();
-            for (Edge e:my.adj(num) ) {
-                if (!isvisit[e.theOther(num)]) {
-                    queue.offer(e.theOther(num));
-                    isvisit[e.theOther(num)]=true;
-                }
-
-            }
-            for (Edge e:my.adj(num)) {
-                my.relax(num,e,c2);
-            }
+        //如果 c1->c1
+        if (c1 == c2) {
+            System.out.print(1+" "+my.getRescueTrams(c2));
         }
-        System.out.print(cnt+" "+my.getNodeWeight(c2));
+        else {
+            Queue<Integer> queue = new LinkedList<Integer>();
+            boolean[] isvisit=new boolean[n];
+            queue.offer(c1);
+            isvisit[c1]=true;
+            while(!queue.isEmpty()){
+                int num=queue.poll();
+                for (Edge e:my.adj(num) ) {
+                    if (!isvisit[e.theOther(num)]) {
+                        queue.offer(e.theOther(num));
+                        isvisit[e.theOther(num)]=true;
+                    }
+
+                }
+                for (Edge e:my.adj(num)) {
+                    if (!e.isRelax()){
+                        my.relax(num,e);
+                        e.setRelax(true);
+                    }
+
+                }
+            }
+            System.out.print(my.getRoads(c2)+" "+my.getRescueTrams(c2));
+
+
+        }
+
 
     }
 }
