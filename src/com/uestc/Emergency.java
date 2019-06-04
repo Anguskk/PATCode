1db0b543e43fe8e263b1fd7fb0e78ec38f058cf2
AngusKK <604202346@qq.com>
2019-06-04 14:04:08 +0800

    Bell-queue modify

--- src/com/uestc/Emergency.java
+++ src/com/uestc/Emergency.java
@@ -6,10 +6,13 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Emergency {
+    public  boolean[] Onqueue;
+    public  Queue<Integer> queue;
     private Node[] vNodes;
     private int[] distTo;
     private int[] rescueTeams;
     private int[] roads;
+
     private ArrayList<Edge>[] adjedges;
     //static int cnt=0;
 
@@ -19,6 +22,9 @@ public class Emergency {
         distTo = new int[V];
         rescueTeams =new int[V];
         roads = new int[V];
+        queue = new LinkedList<Integer>();
+
+        Onqueue = new boolean[V];
         for (int i = 0; i <V ; i++) {
             roads[i] = 0;
         }
@@ -85,6 +91,10 @@ public class Emergency {
     public void relax(int v1,Edge e){
         if (distTo[e.theOther(v1)] > distTo[v1]+e.Edgeweight) {
             distTo[e.theOther(v1)] = distTo[v1]+e.Edgeweight;
+            if (Onqueue[e.theOther(v1)] == false) {
+                queue.offer(e.theOther(e.theOther(v1)));
+                Onqueue[e.theOther(v1)] = true;
+            }
             roads[e.theOther(v1)] = roads [v1];
             rescueTeams[e.theOther(v1)] = rescueTeams[v1]+vNodes[e.theOther(v1)].nodeWeight;
         }
@@ -129,19 +139,15 @@ public class Emergency {
             System.out.print(1+" "+my.getRescueTrams(c2));
         }
         else {
-            Queue<Integer> queue = new LinkedList<Integer>();
-            boolean[] isvisit=new boolean[n];
-            queue.offer(c1);
-            isvisit[c1]=true;
-            while(!queue.isEmpty()){
-                int num=queue.poll();
-                for (Edge e:my.adj(num) ) {
-                    if (!isvisit[e.theOther(num)]) {
-                        queue.offer(e.theOther(num));
-                        isvisit[e.theOther(num)]=true;
-                    }
+            //Queue<Integer> queue = new LinkedList<Integer>();
+            //boolean[] isvisit=new boolean[n];
+            my.queue.offer(c1);
+            my.Onqueue[c1] = true;
+            //isvisit[c1]=true;
+            while(!my.queue.isEmpty()){
+                int num=my.queue.poll();
+                my.Onqueue[num] = false;
 
-                }
                 for (Edge e:my.adj(num)) {
                         my.relax(num,e);
                 }
