77be1eda616f7d93bc9465d6356660dc4e03dee4
Anguskk <604202346@qq.com>
2019-06-08 22:03:38 +0800

    binarySearch

--- src/com/uestc/Radix.java
+++ src/com/uestc/Radix.java
@@ -1,15 +1,12 @@
 package com.uestc;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Radix {
-//    public static HashMap<Character, Integer> charvalue = new HashMap<Character, Integer>();
-//    public void createMap(){
-//        charvalue.put('0',0);
-//        charvalue.put('1',1);
-//    }
-    public static long radix_10_Sum(String s,int radix){
+
+    public static long radix_10_Sum(String s,long radix){
         long ans=0;
         for (int i = 0; i <s.length() ; i++) {
             if (s.charAt(i) >= '0' && s.charAt(i) <= '9' ) {
@@ -21,14 +18,18 @@ public class Radix {
         }
         return  ans;
     }
+
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
         String[] strings=new String[2];
         strings[0]=scanner.next();
         strings[1]=scanner.next();
-        int tag,radix;
+        int tag;
+        long radix;
         tag =scanner.nextInt();
         radix = scanner.nextInt();
+        BigInteger g;
+
         long given,query;
         given = radix_10_Sum(strings[tag-1],radix);
         //2-tag 为另一个string的index
@@ -56,17 +57,56 @@ public class Radix {
         else if (minQuery > given){
             System.out.print("Impossible");
         }
+        // Radix 可能超过36
         else {
-            int i;
-            for (i = minRadix+1; i <= 36 ; i++) {
-                if (radix_10_Sum(strings[2-tag],i) == given) {
-                    System.out.print(i);
+            //int highBound = Integer.max(Math.toIntExact(given),minRadix)+1;
+            //i是进制的遍历  ,最大不超过given  (10)1*given=given
+            long low=minRadix+1;
+            long upperbound=given;
+            long ans=-1;
+            while (low<=upperbound){
+                long mid = (low+upperbound)/2;
+                if (radix_10_Sum(strings[2-tag],mid) >given) {
+                    upperbound= mid;
+                }
+                else if (radix_10_Sum(strings[2-tag],mid) < given) {
+                    low = mid;
+                }
+                else {
+                    ans=mid;
                     break;
                 }
             }
-            if (i == 37) {
+//            for (i = minRadix+1; i <= given ; i++) {
+//                if (radix_10_Sum(strings[2-tag],i) == given) {
+//                    System.out.print(i);
+//                    break;
+//                }
+//            }
+            if (ans != -1) {
+                System.out.print(ans);
+            }
+            else{
                 System.out.print("Impossible");
             }
         }
     }
 }
+//    long  l1=radix_10_Sum(strings[2-tag],i+1000);
+//                if( l1>0&& l1< given){
+//        i=i+999;
+//        continue;
+//        }
+//        else {
+//        long l2=radix_10_Sum(strings[2-tag],i+100);
+//        if(l2>0 && l2< given){
+//        i=i+99;
+//        continue;
+//        }
+//        }
+//
+//        long l3=radix_10_Sum(strings[2-tag],i+10);
+//        if(l3>0 && l3< given){
+//        i=i+9;
+//        continue;
+//        }
